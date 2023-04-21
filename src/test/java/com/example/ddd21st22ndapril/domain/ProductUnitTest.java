4d24a848d4fefd7ad9a421223191804fd5952ea2
Rachana Devi <rachanadevi18@gmail.com>
2023-04-21 15:03:15 +0530

    Update method to be discountPrice instead of anyPRice()

--- src/test/java/com/example/ddd21st22ndapril/domain/ProductUnitTest.java
+++ src/test/java/com/example/ddd21st22ndapril/domain/ProductUnitTest.java
@@ -8,30 +8,30 @@ class ProductUnitTest {
 
     @Test
     void shouldEquateTwoProductsHavingSameName() {
-        Product product = new Product("product 1", anyPrice());
-        Product otherProduct = new Product("product 1", anyPrice());
+        Product product = new Product("product 1", discountPrice());
+        Product otherProduct = new Product("product 1", discountPrice());
 
         assertThat(product).isEqualTo(otherProduct);
     }
 
     @Test
     void shouldNotEquateTwoProductsHavingSameNameInDifferentCases() {
-        Product product = new Product("PRoDuCT 1", anyPrice());
-        Product otherProduct = new Product("product 1", anyPrice());
+        Product product = new Product("PRoDuCT 1", discountPrice());
+        Product otherProduct = new Product("product 1", discountPrice());
 
         assertThat(product).isNotEqualTo(otherProduct);
     }
 
     @Test
     void shouldNotEquateDifferentProducts() {
-        Product product = new Product("product 1", anyPrice());
-        Product otherProduct = new Product("product 2", anyPrice());
+        Product product = new Product("product 1", discountPrice());
+        Product otherProduct = new Product("product 2", discountPrice());
 
         assertThat(product).isNotEqualTo(otherProduct);
     }
 
 
-    private Price anyPrice() {
+    private Price discountPrice() {
         return new Price(10.00);
     }
 }
