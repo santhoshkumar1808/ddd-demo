1a4e99bbcb118eb71b5f9c7e485a0d8b6cb9e1a6
Rachana Devi <rachanadevi18@gmail.com>
2023-04-21 10:26:00 +0530

    Initial commit

---
+++ src/main/java/com/example/ddd21st22ndapril/Ddd21st22ndAprilApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddd21st22ndapril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ddd21st22ndAprilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ddd21st22ndAprilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ddd21st22ndapril/Ddd21st22ndAprilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddd21st22ndapril;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ddd21st22ndAprilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
